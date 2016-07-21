70c1ff810ed7960f3e340b2c3c2852679024a68c
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2016-07-21 16:22:01 -0500

    TWRP additions

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,3 +15,18 @@
 
 # Include board config fragments
 include device/cyanogen/msm8916-common/board/*.mk
+
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_RECOVERY_SWIPE := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+# TWRP Build Flags
+TARGET_RECOVERY_FSTAB := device/yu/lettuce/recovery/etc/twrp.fstab
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_CRYPTO := false
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TARGET_RECOVERY_QCOM_RTC_FIX := true
